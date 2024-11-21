83824e1d937e7fec08da80e04ce04e8566132547
rafittu <rafittu@outlook.com>
2024-11-21 19:12:33 -0300

    fest: test create document controller

---
+++ src/modules/documents/test/mocks/documents.mock.ts
@@ -0,0 +1,33 @@
+import { faker } from '@faker-js/faker';
+import { CreateDocumentDto } from '../../dto/create-document.dto';
+import {
+  DocumentOriginEnum,
+  DocumentTypeEnum,
+} from '../../../../database/schema';
+import { IDocument } from '../../interfaces/documents.interface';
+
+export const MockCreateDocument: CreateDocumentDto = {
+  documentName: faker.finance.accountName(),
+  issuer: faker.person.fullName(),
+  documentOrigin: DocumentOriginEnum.ELECTRONIC,
+  documentType: DocumentTypeEnum.CONTRACT,
+  totalTaxes: parseFloat(faker.finance.amount()),
+  netValue: parseFloat(faker.finance.amount()),
+};
+
+export const MockDocumentFile = {
+  fieldname: 'file',
+  originalname: 'test.pdf',
+  encoding: 'binary',
+  mimetype: 'application/pdf',
+  buffer: Buffer.from('PDF file content here'),
+  size: 1024,
+} as Express.Multer.File;
+
+export const MockIDocument: IDocument = {
+  ...MockCreateDocument,
+  id: faker.string.uuid(),
+  fileUrl: faker.internet.url(),
+  createdAt: faker.date.past(),
+  updatedAt: faker.date.recent(),
+};
