fd155f9ec6b7f62578478db0615e984c3ba7f3f0
rafittu <rafittu@outlook.com>
2024-11-21 15:40:36 -0300

    feat: update drizzle config from json to js

---
+++ drizzle.config.js
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv';
+import { resolve } from 'path';
+
+dotenv.config({ path: resolve(__dirname, '.env') });
+
+export default {
+  schema: './src/database/schema.ts',
+  out: './drizzle/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL,
+  },
+};
