ef84a83af7ccaf3a3e4e4075711300b51c3ab88d
rafittu <rafittu@outlook.com>
2024-11-20 21:12:56 -0300

    feat: add document type

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -8,21 +8,24 @@ import {
 } from 'drizzle-orm/pg-core';
 import { InferSelectModel, InferInsertModel } from 'drizzle-orm';
 
-export const documentOriginEnum = pgEnum('document_origin_enum', [
-  'scanned',
-  'electronic',
+export const DocumentOriginEnum = pgEnum('document_origin_enum', [
+  'SCANNED',
+  'ELECTRONIC',
 ]);
-export const documentTypeEnum = pgEnum('document_type_enum', [
-  'contract',
-  'invoice',
+export type DocumentOriginEnum = 'SCANNED' | 'ELECTRONIC';
+
+export const DocumentTypeEnum = pgEnum('document_type_enum', [
+  'CONTRACT',
+  'INVOICE',
 ]);
+export type DocumentTypeEnum = 'CONTRACT' | 'INVOICE';
 
 export const documents = pgTable('documents', {
   id: uuid('id').defaultRandom().primaryKey(),
   document_name: text('document_name').notNull(),
   issuer: text('issuer').notNull(),
-  document_origin: documentOriginEnum('document_origin').notNull(),
-  document_type: documentTypeEnum('document_type').notNull(),
+  document_origin: DocumentOriginEnum('document_origin').notNull(),
+  document_type: DocumentTypeEnum('document_type').notNull(),
   total_taxes: decimal('total_taxes', { precision: 12, scale: 2 }).notNull(),
   net_value: decimal('net_value', { precision: 12, scale: 2 }).notNull(),
   file_url: text('file_url'),
