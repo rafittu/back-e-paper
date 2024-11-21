19c8b6544ac15fbb53b975025c55f96e8a5d3a95
rafittu <rafittu@outlook.com>
2024-11-20 21:13:18 -0300

    feat: create document dto validation

--- src/modules/documents/dto/create-document.dto.ts
+++ src/modules/documents/dto/create-document.dto.ts
@@ -1,1 +1,28 @@
-export class CreateDocumentDto {}
+import { IsNotEmpty, IsString, IsEnum, IsNumber } from 'class-validator';
+import { DocumentOriginEnum, DocumentTypeEnum } from '../../../database/schema';
+
+export class CreateDocumentDto {
+  @IsNotEmpty()
+  @IsString()
+  document_name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  issuer: string;
+
+  @IsNotEmpty()
+  @IsEnum(DocumentOriginEnum)
+  document_origin: DocumentOriginEnum;
+
+  @IsNotEmpty()
+  @IsEnum(DocumentTypeEnum)
+  document_type: DocumentTypeEnum;
+
+  @IsNotEmpty()
+  @IsNumber()
+  total_taxes: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  net_value: number;
+}
