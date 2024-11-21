11f19969e0e43f629a902010f607943fcef9f82b
rafittu <rafittu@outlook.com>
2024-11-20 22:10:41 -0300

    feat: upload document file to minio

--- src/modules/documents/documents.module.ts
+++ src/modules/documents/documents.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DocumentsService } from './documents.service';
 import { DocumentsController } from './documents.controller';
+import { MinioService } from '../../common/aws/minio.service';
 
 @Module({
   controllers: [DocumentsController],
-  providers: [DocumentsService],
+  providers: [MinioService, DocumentsService],
 })
 export class DocumentsModule {}
--- src/modules/documents/services/create_document.service.ts
+++ src/modules/documents/services/create_document.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { IDocumentsRepository } from '../interfaces/repository.interface';
 import { CreateDocumentDto } from '../dto/create-document.dto';
-import { AppError } from 'src/common/errors/Error';
+import { AppError } from '../../../common/errors/Error';
+import { MinioService } from '../../../common/aws/minio.service';
+import { normalizeFileName } from 'src/modules/utils/document_utils';
 
 @Injectable()
 export class CreateDocumentService {
-  constructor(private readonly documentsRepository: IDocumentsRepository) {}
+  constructor(
+    private readonly documentsRepository: IDocumentsRepository,
+    private readonly minioService: MinioService,
+  ) {}
 
   async execute(dto: CreateDocumentDto, file: Express.Multer.File) {
+    const BUCKET_NAME = process.env.MINIO_BUCKET || '';
+
     if (!file) {
       throw new AppError(
         'documents-service.createDocument',
@@ -25,11 +32,16 @@ export class CreateDocumentService {
     }
 
     try {
-      const fileUrlLocation = 'file url location';
+      const normalizedFileName = normalizeFileName(file.originalname);
+      const uploadedFile = await this.minioService.uploadFile(
+        BUCKET_NAME,
+        normalizedFileName,
+        file.buffer,
+      );
 
       const document = await this.documentsRepository.createDocument(
         dto,
-        fileUrlLocation,
+        uploadedFile.Location,
       );
 
       return document;
