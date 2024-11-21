e898c73331a9adcb8273887575a9bf28487238f4
rafittu <rafittu@outlook.com>
2024-11-21 19:23:32 -0300

    feat: test create document successfully

--- src/modules/documents/test/documents.service.spec.ts
+++ src/modules/documents/test/documents.service.spec.ts
@@ -1,18 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { DocumentsService } from '../documents.service';
+import { CreateDocumentService } from '../services/create_document.service';
+import { MinioService } from '../../../common/aws/minio.service';
+import { IDocumentsRepository } from '../interfaces/repository.interface';
+import {
+  MockCreateDocument,
+  MockDocumentFile,
+  MockIDocument,
+} from './mocks/documents.mock';
 
 describe('DocumentsService', () => {
-  let service: DocumentsService;
+  let createDocumentService: CreateDocumentService;
+  let minioService: MinioService;
+  let documentsRepository: IDocumentsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DocumentsService],
+      providers: [
+        CreateDocumentService,
+        {
+          provide: MinioService,
+          useValue: {
+            uploadFile: jest.fn().mockResolvedValue({
+              Location: 'http://mockurl.com',
+            }),
+          },
+        },
+        {
+          provide: 'IDocumentsRepository',
+          useValue: {
+            createDocument: jest.fn().mockResolvedValue(MockIDocument),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<DocumentsService>(DocumentsService);
+    createDocumentService = module.get<CreateDocumentService>(
+      CreateDocumentService,
+    );
+    minioService = module.get<MinioService>(MinioService);
+    documentsRepository = module.get<IDocumentsRepository>(
+      'IDocumentsRepository',
+    );
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(createDocumentService).toBeDefined();
+    expect(minioService).toBeDefined();
+  });
+
+  describe('create document service', () => {
+    it('should create a document successfully', async () => {
+      const result = await createDocumentService.execute(
+        MockCreateDocument,
+        MockDocumentFile,
+      );
+
+      expect(minioService.uploadFile).toHaveBeenCalledTimes(1);
+      expect(documentsRepository.createDocument).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(MockIDocument);
+    });
   });
 });
