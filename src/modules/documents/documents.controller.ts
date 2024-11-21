ab1f0b24f9e37d185af2914ea629ca629b83aafa
rafittu <rafittu@outlook.com>
2024-11-20 21:24:38 -0300

    fire: remove unsued code

--- src/modules/documents/documents.controller.ts
+++ src/modules/documents/documents.controller.ts
@@ -1,16 +1,11 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
   UseInterceptors,
   UploadedFile,
 } from '@nestjs/common';
 import { CreateDocumentDto } from './dto/create-document.dto';
-import { UpdateDocumentDto } from './dto/update-document.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 import { CreateDocumentService } from './services/create_document.service';
@@ -26,28 +21,5 @@ export class DocumentsController {
     @UploadedFile() file: Express.Multer.File,
   ) {
     return this.createDocumentService.execute(createDocumentDto, file);
-  }
-
-  @Get()
-  findAll() {
-    return this.documentsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.documentsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateDocumentDto: UpdateDocumentDto,
-  ) {
-    return this.documentsService.update(+id, updateDocumentDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.documentsService.remove(+id);
   }
 }
