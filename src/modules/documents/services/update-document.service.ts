6b19b8b3e4f7185c15ccdc376addf8dbb5a7d260
rafittu <rafittu@outlook.com>
2024-11-22 15:49:01 -0300

    feat: update document file name in bucket

--- src/modules/documents/services/update-document.service.ts
+++ src/modules/documents/services/update-document.service.ts
@@ -1,22 +1,31 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { IDocumentsRepository } from '../interfaces/repository.interface';
 import { UpdateDocumentDto } from '../dto/update-document.dto';
-import { IDocument } from '../interfaces/documents.interface';
+import { IDocument, IUpdateDocument } from '../interfaces/documents.interface';
 import { AppError } from '../../../common/errors/Error';
-import { parseDocumentNumbers } from '../../../modules/utils/document_utils';
+import {
+  normalizeFileName,
+  parseDocumentNumbers,
+} from '../../../modules/utils/document_utils';
 import { plainToInstance } from 'class-transformer';
+import { MinioService } from 'src/common/aws/minio.service';
 
 @Injectable()
 export class UpdateDocumentService {
   constructor(
     @Inject('IDocumentsRepository')
     private readonly documentsRepository: IDocumentsRepository,
+    private readonly minioService: MinioService,
   ) {}
 
   async execute(
     id: string,
     updateDocumentDto: UpdateDocumentDto,
   ): Promise<IDocument> {
+    const BUCKET_NAME = process.env.MINIO_BUCKET || '';
+
+    let document: IUpdateDocument = {};
+
     if (updateDocumentDto.totalTaxes < 0 || updateDocumentDto.netValue < 0) {
       throw new AppError(
         'documents-service.updateDocument',
@@ -25,8 +34,30 @@ export class UpdateDocumentService {
       );
     }
 
+    if (updateDocumentDto.documentName) {
+      const existingDocument =
+        await this.documentsRepository.findDocumentById(id);
+
+      const newNormalizedFileName = normalizeFileName(
+        updateDocumentDto.documentName,
+      );
+
+      await this.minioService.renameFile(
+        BUCKET_NAME,
+        existingDocument.bucketFileName,
+        newNormalizedFileName,
+      );
+
+      document.bucketFileName = newNormalizedFileName;
+    }
+
     try {
-      const document = plainToInstance(Object, updateDocumentDto);
+      const documentData = plainToInstance(Object, updateDocumentDto);
+
+      document = {
+        ...document,
+        ...documentData,
+      };
 
       const updatedDocument = await this.documentsRepository.updateDocument(
         id,
