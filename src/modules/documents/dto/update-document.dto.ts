7d17a2b5af0a863056a6b74104732dffce722882
rafittu <rafittu@outlook.com>
2024-11-22 13:20:51 -0300

    feat: update document dto validation

--- src/modules/documents/dto/update-document.dto.ts
+++ src/modules/documents/dto/update-document.dto.ts
@@ -1,4 +1,33 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDocumentDto } from './create-document.dto';
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
+import { DocumentOriginEnum, DocumentTypeEnum } from '../../../database/schema';
+import { Type } from 'class-transformer';
 
-export class UpdateDocumentDto extends PartialType(CreateDocumentDto) {}
+export class UpdateDocumentDto extends PartialType(CreateDocumentDto) {
+  @IsOptional()
+  @IsString()
+  documentName?: string;
+
+  @IsOptional()
+  @IsString()
+  issuer?: string;
+
+  @IsOptional()
+  @IsEnum(DocumentOriginEnum)
+  documentOrigin?: DocumentOriginEnum;
+
+  @IsOptional()
+  @IsEnum(DocumentTypeEnum)
+  documentType?: DocumentTypeEnum;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  totalTaxes?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  netValue?: number;
+}
